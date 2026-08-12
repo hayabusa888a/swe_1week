10e8806759f3985835f5b148482260f0e220dbf1
hayabusa888a <hayato1208soccer176@gmail.com>
2026-08-12 12:53:33 +0900

    day2のfilter_and_aggregateをpandasで書き直す

---
+++ day4/day4_pandas.py
@@ -0,0 +1,23 @@
+#day2で作成しtあfilter_and_aggregate関数をpandasで書き換える
+import pandas as pd
+import argparse
+
+
+df = pd.read_csv('../day2/day2_sample.csv', encoding = 'shift_jis')
+
+def filter_and_aggregate(df, date, min_amount):
+    df = df[(df['支払金額'] >= int(min_amount)) & (df['日にち'] == date)]
+    customer_ids = df['顧客id'].nunique()
+    return customer_ids
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--date')
+parser.add_argument('--min_amount')
+args = parser.parse_args()  
+date = args.date
+min_amount = args.min_amount
+
+print(filter_and_aggregate(df, date, min_amount))
+
+
+
