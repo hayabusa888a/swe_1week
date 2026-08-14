44ec8e8833f62ecc215f99962862c76f0088c062
hayabusa888a <hayato1208soccer176@gmail.com>
2026-08-14 15:04:38 +0900

    sqliteへの出力処理を追加

--- day6-7/day6.py
+++ day6-7/day6.py
@@ -1,10 +1,12 @@
 import pandas as pd
+import sqlite3
 
 class Transaciton_Aggregate:
-    def __init__(self, customer_path, product_path, transaction_path):
+    def __init__(self, customer_path, product_path, transaction_path, db_path):
         self.customer_path = customer_path
         self.product_path = product_path
         self.transaction_path = transaction_path
+        self.db_path = db_path
 
     def load_data(self):
         df_customer = pd.read_csv(self.customer_path)
@@ -24,14 +26,22 @@ class Transaciton_Aggregate:
         df_t_p = df_t_p.groupby(['取引id', 'カテゴリ'])['個数'].sum().reset_index()
         df_t_p = df_t_p.groupby('カテゴリ')['個数'].mean().reset_index()
         return df_t_p
+
+    def save_to_db(self, summary1, summary2):
+        conn = sqlite3.connect(self.db_path)
+        summary1.to_sql('age_summary', conn, if_exists='replace', index=False)
+        summary2.to_sql('category_summary', conn, if_exists='replace', index=False)
+        conn.close()
+    
     
     def run(self):
         df_customer, df_product, df_transaction = self.load_data()
         summary1 = self.aggregate_by_age(df_customer, df_product, df_transaction)
         summary2 = self.aggregate_by_category(df_customer, df_product, df_transaction)
+        self.save_to_db(summary1, summary2)
         return summary1, summary2
 def main ():
-    sales = Transaciton_Aggregate('./sample_data/customers.csv', './sample_data/products.csv', './sample_data/transactions.csv')
+    sales = Transaciton_Aggregate('./sample_data/customers.csv', './sample_data/products.csv', './sample_data/transactions.csv', './output/output.db')
     summary1, summary2 = sales.run()
     print(summary1)
     print(summary2)
